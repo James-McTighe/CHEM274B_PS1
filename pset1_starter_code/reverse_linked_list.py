70e2a1ee33fea38a32dce4ea69be494de1e06e07
James-McTighe <james.mctighe97@gmail.com>
2024-09-13 11:16:44 -0400

    initial commit

---
+++ pset1_starter_code/reverse_linked_list.py
@@ -0,0 +1,9 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverse_linked_list(self, head):
+        # TODO: Implement
+        pass
