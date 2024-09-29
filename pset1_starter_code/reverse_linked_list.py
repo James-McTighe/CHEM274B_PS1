6ad5d7e83a384b4debe6f10539286273e654ca77
James-McTighe <james.mctighe97@gmail.com>
2024-09-29 16:08:35 -0400

    finished exercise 8.1

--- pset1_starter_code/reverse_linked_list.py
+++ pset1_starter_code/reverse_linked_list.py
@@ -4,6 +4,46 @@ class ListNode:
         self.next = next
 
 class Solution:
-    def reverse_linked_list(self, head):
+    @staticmethod
+    def print_list(head):
+        output = ""
+        while head is not None:
+            output += str(head.val)
+            output += " "
+            head = head.next
+        return output
+    
+    @staticmethod
+    def reverse_linked_list(head):
         # TODO: Implement
-        pass
+        curr = head
+        prev = None
+
+        while curr is not None:
+            next_node = curr.next
+
+            curr.next = prev
+
+            prev = curr
+            curr = next_node
+
+        return prev
+    
+
+if __name__ == "__main__":
+
+    # Create a hard-coded linked list:
+    # 1 -> 2 -> 3 -> 4 -> 5
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+
+    # print("Given Linked list:", end="")
+    print(Solution.print_list(head))
+
+    head = Solution.reverse_linked_list(head)
+
+    # print("Reversed Linked List:", end="")
+    print(Solution.print_list(head))
