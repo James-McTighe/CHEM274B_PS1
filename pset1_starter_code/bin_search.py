688f54a397fd28c33658aca7fc799d4411543fbd
James-McTighe <james.mctighe97@gmail.com>
2024-09-30 19:49:25 -0400

    added test file

--- pset1_starter_code/bin_search.py
+++ pset1_starter_code/bin_search.py
@@ -11,22 +11,12 @@ class Solution:
             # print(mid)
 
             if arr[mid] == target:
-                return f"target is located at the {mid} index"
+                return mid
             
             elif arr[mid] < target:
                 low = mid + 1
-                print(f"low = {low}")
 
             else:
                 high = mid - 1
-                print(f"high = {high}")
 
         return f"Element {target} not present within list"
-
-
-a = Solution()
-
-b = [x for x in range(1010)]
-
-
-print(a.binary_search(b,450))
