8b72f3020768839ba6e3d89c9e93935ab9505581
James-McTighe <james.mctighe97@gmail.com>
2024-09-30 19:24:01 -0400

    worked on problem 10.2

--- pset1_starter_code/bin_search.py
+++ pset1_starter_code/bin_search.py
@@ -1,4 +1,34 @@
+import math
+
 class Solution:
     def binary_search(self, arr, target):
-        # TODO: Implement
-        pass
+        low = arr[0]
+        high = arr[-1]
+
+        print(low)
+        print(high)
+
+        # while low <= high:
+
+        #     mid = low + high // 2
+        #     print(mid)
+
+        #     if arr[mid] == target:
+        #         return mid
+            
+        #     elif arr[mid] < target:
+        #         low = mid + 1
+
+        #     else:
+        #         high = mid - 1
+
+        return f"Element {target} not present within list"
+
+
+a = Solution()
+
+b = [x for x in range(101)]
+
+print(b[45])
+
+a.binary_search(b,45)
