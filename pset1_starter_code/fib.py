3d1288d688484a7b34499a518586870fa4c84535
James McTighe <james.mctighe97@gmail.com>
2024-09-30 17:11:27 -0400

    started work on problem 10

--- pset1_starter_code/fib.py
+++ pset1_starter_code/fib.py
@@ -1,8 +1,19 @@
 class Solution:
     def fib_recursive(self, n):
-        # TODO: Implement
-        pass
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        print(n)
+        return self.fib_recursive(n-1) + self.fib_recursive(n-2)
+        
+
 
     def fib_iterative(self, n):
         # TODO: Implement
         pass
+
+if __name__ == "__main__":
+
+    a = Solution()
+    print(a.fib_recursive(5))
