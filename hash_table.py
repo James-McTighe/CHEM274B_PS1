0e1fd6a33b54fff5893f4dc1cf6b585ceda2fdab
James-McTighe <james.mctighe97@gmail.com>
2024-10-03 08:03:31 -0400

    worked on problem 12

--- hash_table.py
+++ hash_table.py
@@ -10,20 +10,19 @@ class HashTable:
         return (sum_total * 31) % self.size
 
     def insert(self, key, value):
-        # TODO: Implement for 12.2
-        pass
+        destination = self.table[key]
+        # destination.append(value)
+        destination.insert(0,value)
+        ## TODO: implement collision resolution
 
     def get(self, key):
-        # TODO: Implement for 12.2
-        pass
+        return self.table[key]
+    ## TODO: implement collision resolution
 
     def remove(self, key):
-        # TODO: Implement for 12.2
-        pass
+        self.table.remove(self._hash(key))
+        ## TODO: implement collision resolution      
 
-    def load_factor(self):
-        # TODO Implement for 12.3
-        pass
             
 class DynamicHashTable(HashTable):
     def __init__(self, size):
@@ -32,8 +31,15 @@ class DynamicHashTable(HashTable):
         self.load_factor_threshold = 0.7
 
     def calculate_load_factor(self):
-        # TODO: Implement for 12.3
-        pass
+        count = 0
+        for _ in self.table:
+            if not _:
+                continue
+            else:
+                for x in _ :
+                    count += 1
+
+        return count
 
     def insert(self, key, value):
         # TODO: Implement for 12.3
@@ -46,4 +52,21 @@ class DynamicHashTable(HashTable):
     def resize(self):
         # TODO: Implement for 12.3
         pass
+
+
+# Testing
+a = HashTable(10)
+print(a.load_factor())
+print(a.table)
+b = []
+
+a.insert(0,10)
+a.insert(0,20)
+a.insert(4,5)
+print(a.table)
+
+print(a.table)
+print(a.load_factor())
+
+
 
